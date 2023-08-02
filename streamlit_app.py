abb67038936f02c79a761189e4254f50447bd92b
SaraKap <sara.kapuscinska@gmail.com>
2023-08-02 16:35:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ except URLError as e:
      
 
 streamlit.header("The fruit load list contains:")
+my_cur=my_cnx.cursor()
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
